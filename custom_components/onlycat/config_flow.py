bbcb81128940b672b5bc70085d0f360429a6ee72
Alexander Landmesser <alexander@landmesser.online>
2026-03-31 20:45:25 +0200

    properly catch auth errors, in this case a dict is returned while on successful auth, a list is returned

--- custom_components/onlycat/config_flow.py
+++ custom_components/onlycat/config_flow.py
@@ -212,10 +212,11 @@ class OnlyCatFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         """Validate connection."""
         await client.connect()
         response = await client.send_message("getDevices", {"subscribe": False})
-        if "code" not in response:
-            error_msg = f"Unexpected response from OnlyCat API: 'code' field is missing in {response}"
-            raise OnlyCatApiClientError(error_msg)
-        if response["code"] == ONLYCAT_API_STATUS_CODES["UNAUTHORIZED"]:
+        if (
+            type(response) is dict
+            and "code" in response
+            and response["code"] == ONLYCAT_API_STATUS_CODES["UNAUTHORIZED"]
+        ):
             error_msg = "Invalid access token"
             raise OnlyCatApiClientAuthenticationError(error_msg)
         await client.disconnect()
